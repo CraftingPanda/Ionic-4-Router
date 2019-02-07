804755bf5a17c72efdfebad40278d85eefe80c24
CraftingPanda <46329520+CraftingPanda@users.noreply.github.com>
2019-02-07 16:56:52 +0100

    Add files via upload

---
+++ src/app/seite/seite.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SeitePage } from './seite.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SeitePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [SeitePage]
+})
+export class SeitePageModule {}
---
+++ src/app/seite/seite.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seite',
+  templateUrl: './seite.page.html',
+  styleUrls: ['./seite.page.scss'],
+})
+export class SeitePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
